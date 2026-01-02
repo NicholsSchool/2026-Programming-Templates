4298641d164b7d4882615cb0952556866839280f
James Burnett <james.c.burnett@icloud.com>
2026-01-02 12:56:57 -0800

    added and tested drivetrain functionality with sim

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,7 +73,10 @@ public final class Constants {
     public static final double TRACK_WIDTH_Y = Units.inchesToMeters(24.5);
     public static final double DRIVE_BASE_RADIUS =
         Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
-    public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
+    
+        public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
+
+    public static final double LOW_GEAR_SCALER = 0.6;
     
   }
 
