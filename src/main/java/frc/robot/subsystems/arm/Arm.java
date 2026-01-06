2b286d80e3c46e3c22672d75032f20de831ec620
emily <hiller.emily@nicholsschool.org>
2026-01-06 18:42:23 -0500

    commiting AGAIN because james wanted the one change that didn't commit last time for some reason, just arm stuff

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -13,6 +13,7 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
@@ -28,15 +29,28 @@ public class Arm extends SubsystemBase {
     private ArmState state;
     private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
     private ArmFeedforward armFF = new ArmFeedforward(Constants.ArmConstants.ARM_FF_KS, Constants.ArmConstants.ARM_FF_KG, Constants.ArmConstants.ARM_FF_KV, Constants.ArmConstants.ARM_FF_KA);
-    private double voltageCMDFF = 0.0;
+    private double voltageCmdFF = 0.0;
+    private double voltageCmdPid = 0.0;
+    private double targetAngleDeg = 0.0;
+    private double manualInput = 0.0;
+    private double voltageCommand = 0.0;
+    private double previousVoltage = 0.0;
+
+    private boolean reachedTargetPos = false;
 
     private final ProfiledPIDController PIDcontroller = new ProfiledPIDController(Constants.ArmConstants.ARM_P, Constants.ArmConstants.ARM_I, Constants. ArmConstants.ARM_D, 
-    new TrapezoidProfile.Constraints(Constants.ArmConstants.ARM_MAX_VEL, Constants.ArmConstants.ARM_MAX_ACCEL));
+    new TrapezoidProfile.Constraints(0, 0));
 
     private static final LoggedTunableNumber armKg = new LoggedTunableNumber("Arm/kG");
     private static final LoggedTunableNumber armKv = new LoggedTunableNumber("Arm/kV");
     private static final LoggedTunableNumber armKa = new LoggedTunableNumber("Arm/kA");
 
+    private static final LoggedTunableNumber armKp = new LoggedTunableNumber("Arm/kP");
+    private static final LoggedTunableNumber armKi = new LoggedTunableNumber("Arm/kI");
+    private static final LoggedTunableNumber armKd = new LoggedTunableNumber("Arm/kD");
+
+    private static final LoggedTunableNumber positionToleranceDeg =
+      new LoggedTunableNumber("Arm/PositionToleranceDeg");
     private static final LoggedTunableNumber armMaxAccelerationRad =
         new LoggedTunableNumber("Arm/armMaxAccelerationRad");
     private static final LoggedTunableNumber armMaxVelocityRad =
@@ -58,8 +72,19 @@ public class Arm extends SubsystemBase {
         armKg.initDefault(Constants.ArmConstants.ARM_FF_KG);
         armKv.initDefault(Constants.ArmConstants.ARM_FF_KV);
         armKa.initDefault(Constants.ArmConstants.ARM_FF_KA);
+
+        armKp.initDefault(Constants.ArmConstants.ARM_P);
+        armKi.initDefault(Constants.ArmConstants.Arm_I);
+        armKd.initDefault(Constants.ArmConstants.ARM_D);
         armMaxAccelerationRad.initDefault(Constants.ArmConstants.ARM_MAX_ACCEL);
         armMaxVelocityRad.initDefault(Constants.ArmConstants.ARM_MAX_VEL);
+        positionToleranceDeg.initDefault(2.0);
+
+        PIDcontroller.setP(armKp.get());
+        PIDcontroller.setI(armKi.get());
+        PIDcontroller.setD(armKd.get());
+        new TrapezoidProfile.Constraints(armMaxVelocityRad.get(), armMaxAccelerationRad.get());
+        PIDcontroller.setTolerance(Units.degreesToRadians(positionToleranceDeg.get()));
     }
 
 
@@ -67,8 +92,10 @@ public class Arm extends SubsystemBase {
     public void periodic(){
 
         io.updateInputs(inputs);
+        Logger.processInputs("Arm", inputs);
+        updateTunables();
 
-        voltageCMDFF = armFF.calculate(inputs.angleRads, Constants.ArmConstants.ARM_MAX_VEL * -1
+        voltageCmdFF = armFF.calculate(inputs.angleRads, Constants.ArmConstants.ARM_MAX_VEL * -1
         //manual);
         );
         switch(state){
@@ -77,11 +104,65 @@ public class Arm extends SubsystemBase {
             case POS:
             break;
         } 
+
+        if (DriverStation.isDisabled()) {}
+
+        switch (armState) {
+          case MANUAL:
+            voltageCmdPid = 0.0;
+            voltageCmdFF =
+                armFF.calculate(
+                    inputs.angleRads, Constants.ArmConstants.ARM_MAX_VEL * softLimit(manualInput));
+            break;
+          case kGoToPos:
+            voltageCmdPid = PIDcontroller.calculate(inputs.angleRads);
+            voltageCmdFF = armFF.calculate(inputs.angleRads, PIDcontroller.getSetpoint().velocity);
+    
+            if (!reachedTargetPos) {
+              reachedTargetPos = PIDcontroller.atGoal();
+              if (reachedTargetPos) {
+                System.out.println("Arm Move To Pos Reached Goal!");
+                timerMoveToPose.stop();
+              }
+            }
+            break;
+        }
+            voltageCommand = voltageCmdPid + voltageCmdFF;
+            io.setVoltage(voltageCommand);
     }
 
-    public void setTargetangle(){
-        
+    public void setTargetangle(double targetAngleDeg){
+        this.targetAngleDeg = targetAngleDeg;
+
     }
+
+    public void setGoToPosition(){
+        this.manualInput = 0.0;
+        armState = ArmState.POS;
+    }
+
+    private void updateTunables() {
+        // Update from tunable numbers
+        if (armKg.hasChanged(hashCode())
+            || armKv.hasChanged(hashCode())
+            || armKa.hasChanged(hashCode())) {
+          ARM_FF = new ArmFeedforward(0.0, armKg.get(), armKv.get(), armKa.get());
+        }
+        if (armMaxVelocityRad.hasChanged(hashCode())
+            || armMaxAccelerationRad.hasChanged(hashCode())
+            || positionToleranceDeg.hasChanged(hashCode())
+            || armKp.hasChanged(hashCode())
+            || armKi.hasChanged(hashCode())
+            || armKd.hasChanged(hashCode())) {
+          armPidController.setP(armKp.get());
+          armPidController.setI(armKi.get());
+          armPidController.setD(armKd.get());
+          armPidController.setConstraints(
+              new TrapezoidProfile.Constraints(armMaxVelocityRad.get(), armMaxAccelerationRad.get()));
+          armPidController.setTolerance(Units.degreesToRadians(positionToleranceDeg.get()));
+        }
+      }
+    
 
 
 }
